e28276adb5e958916a43a8b2834b1065eaea4d4f
Guilherme Marques <mr.gmrsouza@gmail.com>
2026-08-26 23:56:14 -0300

    feat: add custom exception classes for duplicate resources and not found errors

--- src/main/java/user/exception/ResourceNotFoundException.java
+++ src/main/java/user/exception/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package user.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-  public ResourceNotFoundException(String message) {
-    super(message);
-  }
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
