473862da60185b4063a4d64aaa6fc3d3e790a83a
ryanlbpimentel <ryanlbpimentel@alu.ufc.br>
2026-08-29 00:46:25 -0300

    feat: configura banco de dados e adiciona DTO de transação Pix

---
+++ src/main/java/com/gc/sistem_pix/pix/dto/CreatePixTransaction.java
@@ -0,0 +1,27 @@
+package com.gc.sistem_pix.pix.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record CreatePixTransaction(
+        @NotNull
+        UUID contaOrigemId,
+
+        @NotNull
+        UUID contaDestinoId,
+
+        @NotNull
+        @Positive
+        BigDecimal valor,
+
+        @Size(max = 255)
+        String descricao
+
+        // @NotNull
+        // Boolean transacaoAgendada
+) {
+}
